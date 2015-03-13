ff76f512f39ab402336f45f92abc0f0591879c2f
khunjuice <khunjuice@gmail.com>
2015-03-13 13:48:23 +0700

    add lab1

---
+++ PythonApplication1/lab1.py
@@ -0,0 +1,62 @@
+import sys
+from PySide.QtCore import*
+from PySide.QtGui import*
+
+class simple_drawing_window(QWidget):
+    def __init__(self):
+        super().__init__(None)
+        self.setWindowTitle("Simple Drawing")
+        self.rabbit = QImage("images/rabbit.png")
+    def paintEvent(self,e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(0,0,0))
+        p.setBrush(QColor(0,127,0))
+        p.drawPolygon([QPoint(70,100),QPoint(100,110),QPoint(130,100),QPoint(100,150)])
+        
+        p.setPen(QColor(255,127,0))
+        p.setBrush(QColor(255,127,0))
+
+        p.drawPie(50,150,100,100,0,180*16)
+        p.drawPolygon([QPoint(50,200),QPoint(150,200),QPoint(100,400)])
+        p.drawImage(QRect(200,100,320,320),self.rabbit)
+        p.end()
+class simple_drawing_window1(QWidget):
+    def __init__(self):
+        super().__init__(None)
+        self.setWindowTitle("Simple Drawing")
+        self.rabbit = QImage("images/rabbit.png")
+    def paintEvent(self,e):
+        p = QPainter()
+        p.begin(self)
+
+        
+        p.setPen(QColor(255,255,0))
+        p.setBrush(QColor(255,255,0))
+
+        p.drawPolygon([QPoint(150,200),QPoint(50,200),QPoint(150,400),QPoint(50,400)])
+        p.end()
+class simple_drawing_window2(QWidget):
+    def __init__(self):
+        super().__init__(None)
+        self.setWindowTitle("Simple Drawing")
+        self.rabbit = QImage("images/rabbit.png")
+    def paintEvent(self,e):
+        p = QPainter()
+        p.begin(self)
+
+        
+        p.setPen(QColor(255,55,140))
+        p.setBrush(QColor(255,55,140))
+
+        p.drawPolygon([QPoint(100,100),QPoint(100,200),QPoint(300,200)])
+        p.end()
+def main():
+    app= QApplication(sys.argv)
+    w = simple_drawing_window2()
+    w.show()
+
+    return app.exec_()
+if __name__ == "__main__":
+    sys.exit(main())
